08e2e2f999563310003c990e255facce2f6e5197
deepthins08 <deepthins.xworkz@gmail.com>
2023-10-28 16:27:29 +0530

    Opener is the main class

---
+++ Variables/Example2/Pendrive.java
@@ -0,0 +1,18 @@
+class Pendrive{
+	
+	static String brand;
+	static double cost;
+	static double storage;
+	static String color;
+	static double weight;
+	
+	static void penDriveInfo(){
+		
+		System.out.println("Pendrive specification");
+		System.out.println("pendrive brand is: "+brand);
+		System.out.println("pendrive Cost is: "+cost);
+		System.out.println("pendrive storage is "+storage);
+		System.out.println("pendrive color is "+color);
+		System.out.println("pendrive wight is: "+weight);
+	}
+}
