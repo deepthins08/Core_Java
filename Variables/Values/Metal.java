34e7ef94633399262140a5f00b4a3756f2689820
deepthins08 <deepthins.xworkz@gmail.com>
2023-10-28 16:43:52 +0530

    variables

---
+++ Variables/Values/Metal.java
@@ -0,0 +1,20 @@
+class Metal{
+	
+	static double weight=10;
+	static String type="Gold";
+	static String design="ring";
+	static double cost=55000;
+	static String color="gold";
+	
+	static void metalInfo(){
+		
+		System.out.println("Information of Metal is starting");
+		System.out.println("Metal Details");
+		System.out.println("Metal weight: "+weight);
+		System.out.println("Metal type is: "+type);
+		System.out.println("Metal design is "+design);
+		System.out.println("Metal cost is "+cost);
+		System.out.println("Metal color : "+color);
+		System.out.println("Information of Metal is ending");
+	}
+}
