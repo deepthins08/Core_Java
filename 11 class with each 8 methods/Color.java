e0aa7eade05264cbdb398b2646ae740ae0ebf89f
deepthins08 <deepthins.xworkz@gmail.com>
2023-10-28 15:35:56 +0530

    Deepthi is main class

---
+++ 11 class with each 8 methods/Color.java
@@ -0,0 +1,44 @@
+class Color{
+	
+	static void pink(){
+		
+		System.out.println("pink color");
+	}
+	
+	static void purple(){
+		
+		System.out.println("purple color");
+	}
+	
+	static void red(){
+		
+		System.out.println("red color");
+	}
+	
+	static void green(){
+		
+		System.out.println("green color");
+	}
+	
+	static void white(){
+		
+		System.out.println("white color");
+	}
+	
+	static void black(){
+		
+		System.out.println("black color");
+	}
+	
+	static void yellow(){
+		
+		System.out.println("yellow color");
+	}
+	
+	static void violet(){
+		
+		System.out.println("violet color");
+	}
+	
+	
+}
