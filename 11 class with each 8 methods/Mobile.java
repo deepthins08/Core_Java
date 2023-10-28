31fde3d7953059960e88c3e2e399845835dfa3b1
deepthins08 <deepthins.xworkz@gmail.com>
2023-10-28 15:38:23 +0530

    Deepthi is main class

---
+++ 11 class with each 8 methods/Mobile.java
@@ -0,0 +1,42 @@
+class Mobile{
+	
+	static void songs(){
+		
+		System.out.println("We can play songs");
+	}
+	
+	static void call(){
+		
+		System.out.println("we can call someone");
+	}
+	
+	static void whatsApp(){
+		
+		System.out.println("many poeple use whatsApp");
+	}
+	
+	static void instaGram(){
+		
+		System.out.println("instagram is a entertainment app");
+	}
+	
+	static void videoCall(){
+		
+		System.out.println("we can make videoCalls");
+	}
+	
+	static void message(){
+		
+		System.out.println("we can exchange messages");
+	}
+	
+	static void mms(){
+		
+		System.out.println("we can also able to send mms");
+	}
+	
+	static void fileTransfer(){
+		
+		System.out.println("we can share files");
+	}
+}
