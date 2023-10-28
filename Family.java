74ea1659653077e8f08d9f3eb74d5dc4bc38d6fb
deepthins08 <deepthins.xworkz@gmail.com>
2023-10-28 14:10:34 +0530

    Family class code

---
+++ Family.java
@@ -0,0 +1,5 @@
+class Family{
+	public static void main(String[] args){
+		System.out.println("1.Father:Shrinivasu\nOccupation:Farmer\n2.Mother:Lakshmi\nOccupation:Home Maker\n3.Younger Brother:Vamshi Krishna\nOccupation:Student\nGrand Father:Purushottam\nOccupation:Farmer\nGrand Mother:Ananta Lakshmi\nOccupation:Home Maker");
+	}
+}
