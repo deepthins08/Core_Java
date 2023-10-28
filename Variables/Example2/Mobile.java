12c1f7ef3e2c98bf4e0c31071357eda18aec2460
deepthins08 <deepthins.xworkz@gmail.com>
2023-10-28 16:26:10 +0530

    Opener is the main class

---
+++ Variables/Example2/Mobile.java
@@ -0,0 +1,18 @@
+class Mobile{
+	
+	static String brand;
+	static double cost;
+	static double ram;
+	static double battery;
+	static double storage;
+	
+	static void specInfo(){
+		
+		System.out.println("Mobile specification");
+		System.out.println("Mobile brand is: "+brand);
+		System.out.println("Mobile Cost is: "+cost);
+		System.out.println("Mobile RAM is "+ram);
+		System.out.println("Mobile battery Capacity is "+battery);
+		System.out.println("Mobile Storage is: "+storage);
+	}
+}
