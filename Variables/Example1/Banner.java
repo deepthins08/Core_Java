689657905570bbef530b64faba3d5514f43128d4
deepthins08 <deepthins.xworkz@gmail.com>
2023-10-28 16:08:30 +0530

    Starter is a main class

---
+++ Variables/Example1/Banner.java
@@ -0,0 +1,22 @@
+class Banner{
+	
+	static String name;
+	static double quantity;
+	static String title;
+	
+	static void setName(String name){
+		
+		Banner.name=name;
+	}
+	
+	static void setQuantity(double quantity){
+		
+		Banner.quantity=quantity;
+	}
+	
+	static void setTitle(String title){
+		
+		Banner.title=title;
+	}
+
+}
