6e95a0ee20d53c9203af3fdd1066344806b13eab
deepthins08 <deepthins.xworkz@gmail.com>
2023-10-28 16:44:34 +0530

    variables

---
+++ Variables/Values/Socket.java
@@ -0,0 +1,21 @@
+class Socket{
+	
+	static String brand="Digway";
+	static double cost=299;
+	static double quantity=2;
+	static String type="threepin";
+	static String color="Beige";
+	
+	
+	static void socketInfo(){
+		
+		System.out.println("Information of socket is starting");
+		System.out.println("socket Details");
+		System.out.println("socket brand : "+brand);
+		System.out.println("socket cost is: "+cost);
+		System.out.println("socket quantity: "+quantity);
+		System.out.println("socket type is "+type);
+		System.out.println("socket color is "+color);
+		System.out.println("Information of socket is ending");
+	}
+}
