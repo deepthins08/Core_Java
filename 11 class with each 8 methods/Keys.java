6681ff265b79212e47d6c4d29eea58a38e2107f1
deepthins08 <deepthins.xworkz@gmail.com>
2023-10-28 15:37:33 +0530

    Deepthi is main class

---
+++ 11 class with each 8 methods/Keys.java
@@ -0,0 +1,126 @@
+class Keys{
+	
+	public static void main(String[] keyfunctions){
+		
+		Key.keyOpen();
+		Key.keyClose();
+		Key.keyRust();
+		Key.keyLost();
+		Key.keyFound();
+		Key.keyThrow();
+		Key.keyBought();
+		Key.keyStarter();
+		
+		System.out.println("\n");
+		
+		Paint.brush();
+		Paint.coloring();
+		Paint.designing();
+		Paint.drawing();
+		Paint.outLine();
+		Paint.reshape();
+		Paint.erasing();
+		Paint.selling();
+		
+		System.out.println("\n");
+		
+		Tv.movies();
+		Tv.shows();
+		Tv.volumeUp();
+		Tv.volumeDown();
+		Tv.channels();
+		Tv.video();
+		Tv.audio();
+		Tv.power();
+		
+		System.out.println("\n");
+		
+		Car.seat();
+		Car.belt();
+		Car.tyre();
+		Car.steering();
+		Car.gear();
+		Car.ac();
+		Car.roofTop();
+		Car.engine();
+		
+		System.out.println("\n");
+		
+		Street.road();
+		Street.car();
+		Street.bike();
+		Street.cycle();
+		Street.people();
+		Street.van();
+		Street.bus();
+		Street.scooty();
+		
+		System.out.println("\n");
+		
+		Building.room();
+		Building.stairs();
+		Building.lift();
+		Building.corridor();
+		Building.balcany();
+		Building.groundFloor();
+		Building.wall();
+		Building.backYard();
+		
+		System.out.println("\n");
+		
+		Juice.orangeJuice();
+		Juice.appleJuice();
+		Juice.pineAppleJuice();
+		Juice.dragonFruitJuice();
+		Juice.grapesJuice();
+		Juice.avacadoJuice();
+		Juice.strawBerryJuice();
+		Juice.blackBerryJuice();
+		
+		System.out.println("\n");
+		
+		Fruits.orange();
+		Fruits.apple();
+		Fruits.pineApple();
+		Fruits.dragonFruit();
+		Fruits.grapes();
+		Fruits.avacado();
+		Fruits.strawBerry();
+		Fruits.blackBerry();
+		
+		System.out.println("\n");
+		
+		Mobile.songs();
+		Mobile.call();
+		Mobile.instaGram();
+		Mobile.whatsApp();
+		Mobile.videoCall();
+		Mobile.message();
+		Mobile.mms();
+		Mobile.fileTransfer();
+		
+		System.out.println("\n");
+		
+		Color.pink();
+		Color.purple();
+		Color.red();
+		Color.green();
+		Color.white();
+		Color.black();
+		Color.yellow();
+		Color.violet();
+		
+		System.out.println("\n");
+		
+		Laptop.screen();
+		Laptop.apps();
+		Laptop.hardware();
+		Laptop.software();
+		Laptop.keyBoard();
+		Laptop.mouse();
+		Laptop.desktop();
+		Laptop.storage();
+		
+	}
+
+}
