4533cbef1827cbe3965cb9efa433aa275246a8ff
deepthins08 <deepthins.xworkz@gmail.com>
2023-10-28 16:54:06 +0530

    methods

---
+++ methods/Jewellary.java
@@ -0,0 +1,31 @@
+class Jewellary{
+	
+	static void diamond(){
+		System.out.println("This is Diamond jewel");
+	}
+	
+	static void pearl(){
+		System.out.println("This is pearls jewel");
+	}
+	
+	static void ruby(){
+		System.out.println("This is ruby jewel");
+	}
+	
+	static void gold(){
+		System.out.println("This is gold jewel");
+	}
+	
+	static void silver(){
+		System.out.println("This is silver jewel");
+	}
+	
+	public static void main(String[] args){	
+		System.out.println("This is Main Class");
+		diamond();
+		pearl();
+		ruby();
+		gold();
+		silver();
+	}
+}
