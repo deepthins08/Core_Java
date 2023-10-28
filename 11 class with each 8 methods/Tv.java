5c94d178235a8723b6135801f2df2b14048ca7b5
deepthins08 <deepthins.xworkz@gmail.com>
2023-10-28 15:39:58 +0530

    Deepthi is main class

---
+++ 11 class with each 8 methods/Tv.java
@@ -0,0 +1,42 @@
+class Tv{
+	
+	static void movies(){
+		
+		System.out.println("watching movies");
+	}
+	
+	static void shows(){
+		
+		System.out.println("watching shows");
+	}
+	
+	static void volumeUp(){
+		
+		System.out.println("tv volume up");
+	}
+	
+	static void volumeDown(){
+		
+		System.out.println("tv volume down");
+	}
+	
+	static void channels(){
+		
+		System.out.println("tv channels");
+	}
+	
+	static void video(){
+		
+		System.out.println("watching video");
+	}
+	
+	static void audio(){
+		
+		System.out.println("tv audio quality is good");
+	}
+	
+	static void power(){
+		
+		System.out.println("powering the tv");
+	}
+}
