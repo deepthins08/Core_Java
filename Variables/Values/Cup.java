12c30a12f72026a3558c723fb21e53d152dd53ab
deepthins08 <deepthins.xworkz@gmail.com>
2023-10-28 16:40:03 +0530

    variables

---
+++ Variables/Values/Cup.java
@@ -0,0 +1,20 @@
+class Cup{
+	
+	static String size="Medium";
+	static double cost=150.0;
+	static String color="pink";
+	static double quantity=100;
+	static String type="marbel";
+	
+	static void cupInfo(){
+		
+		System.out.println("Information of Cup at starting");
+		System.out.println("Cup Details");
+		System.out.println("Cup size: "+size);
+		System.out.println("Cup Cost is: "+cost);
+		System.out.println("Cup color is "+color);
+		System.out.println("Cup quantity is "+quantity);
+		System.out.println("Cup type : "+type);
+		System.out.println("Information of Cup at ending");
+	}
+}
