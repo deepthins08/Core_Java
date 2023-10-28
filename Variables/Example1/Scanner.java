d048587708db81b7903ce15cc7d648c597d5b9d9
deepthins08 <deepthins.xworkz@gmail.com>
2023-10-28 16:10:24 +0530

    Starter is a main class

---
+++ Variables/Example1/Scanner.java
@@ -0,0 +1,21 @@
+class Scanner{
+	
+	static String platformName;
+	static double amount;
+	static String scan;
+	
+	static void setPlatfromName(String platformName){
+		
+		Scanner.platformName=platformName;
+	}
+	
+	static void setAmount(double amount){
+		
+		Scanner.amount=amount;
+	}
+	
+	static void setScan(String scan){
+		
+		Scanner.scan=scan;
+	}
+}
