6d9f1d908ad07fcd4011f453e9f5e7181b450b1f
deepthins08 <deepthins.xworkz@gmail.com>
2023-10-28 15:36:42 +0530

    Deepthi is main class

---
+++ 11 class with each 8 methods/Juice.java
@@ -0,0 +1,42 @@
+class Juice{
+	
+	static void orangeJuice(){
+		
+		System.out.println("orangeJuice");
+	}
+	
+	static void appleJuice(){
+		
+		System.out.println("appleJuice");
+	}
+	
+	static void pineAppleJuice(){
+		
+		System.out.println("pineAplleJuice");
+	}
+	
+	static void dragonFruitJuice(){
+		
+		System.out.println("dragonFruitJuice");
+	}
+	
+	static void grapesJuice(){
+		
+		System.out.println("grapesJuice");
+	}
+	
+	static void avacadoJuice(){
+		
+		System.out.println("avacadoJuice");
+	}
+	
+	static void strawBerryJuice(){
+		
+		System.out.println("strawBerryJuice");
+	}
+	
+	static void blackBerryJuice(){
+		
+		System.out.println("blackBerryJuice");
+	}
+}
