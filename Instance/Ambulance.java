e74b3c01744bee9181621a23088a605e5e479027
deepthins08 <deepthins.xworkz@gmail.com>
2023-10-28 17:19:05 +0530

    Instance

---
+++ Instance/Ambulance.java
@@ -0,0 +1,14 @@
+class Ambulance{
+	
+	String ambulanceType;
+	String hospital;
+	int noOFSeats;
+	int emergencyNo;
+	String location;
+	
+	Ambulance(){
+		
+		System.out.println("Ambulance is Created");
+	}
+	
+}
