15146af2873cb93e7aa177c614d396b559ef3faa
deepthins08 <deepthins.xworkz@gmail.com>
2023-10-28 15:37:59 +0530

    Deepthi is main class

---
+++ 11 class with each 8 methods/Laptop.java
@@ -0,0 +1,43 @@
+class Laptop{
+	
+	static void screen(){
+		
+		System.out.println("lap screen");
+	}
+	
+	static void apps(){
+		
+		System.out.println("pc application");
+	}
+	
+	static void hardware(){
+		
+		System.out.println("hardware of pc");
+	}
+	
+	static void software(){
+		
+		System.out.println("software of pc");
+	}
+	
+	static void keyBoard(){
+		
+		System.out.println("keyboard of the pc");
+	}
+	
+	static void mouse(){
+		
+		System.out.println("mouse");
+	}
+	
+	static void desktop(){
+		
+		System.out.println("desktop");
+	}
+	
+	static void storage(){
+		
+		System.out.println("storage");
+	}
+	
+	}
