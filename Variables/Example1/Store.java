d158a984560fe73a131caa1fe2acef6358c76321
deepthins08 <deepthins.xworkz@gmail.com>
2023-10-28 16:11:42 +0530

    Starter is a main class

---
+++ Variables/Example1/Store.java
@@ -0,0 +1,23 @@
+class Store{
+	
+	static String storeName;
+	static String address;
+	static long mobile;
+	
+	
+	static void setStoreName(String storeName){
+		
+		Store.storeName=storeName;
+	}
+	
+	static void setAddress(String address){
+		
+		Store.address=address;
+	}
+	
+	static void setMobile(long mobile){
+		
+		Store.mobile=mobile;
+	}
+	
+}
