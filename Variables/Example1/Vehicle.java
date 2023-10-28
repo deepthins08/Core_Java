5e9d14e5a6cba4e7fcb8798ab9de09d9cd74f528
deepthins08 <deepthins.xworkz@gmail.com>
2023-10-28 16:14:55 +0530

    Starter is a main class

---
+++ Variables/Example1/Vehicle.java
@@ -0,0 +1,23 @@
+class Vehicle{
+	
+	static String vehicleName;
+	static String brand;
+	static String vehicleType;
+	
+	static void setVehicleName(String vehicleName){
+		
+		Vehicle.vehicleName=vehicleName;
+		
+	}
+	
+	static void setBrand(String brand){
+		
+		Vehicle.brand=brand;
+	}
+	
+	static void setVehicleType(String vehicleType){
+		
+		Vehicle.vehicleType=vehicleType;
+	}
+	
+}
