1a2e9c14b27d4fa8a1234664b00dd60823261666
deepthins08 <deepthins.xworkz@gmail.com>
2023-10-28 16:57:15 +0530

    methods

---
+++ methods/Tap.java
@@ -0,0 +1,31 @@
+class Tap{
+	
+	static void pillarTap(){
+		System.out.println("This is pillarTap");
+	}
+	
+	static void mixerTap(){
+		System.out.println("This is mixerTap");
+	}
+	
+	static void washerTap(){
+		System.out.println("This is washerTap");
+	}
+	
+	static void discTap(){
+		System.out.println("This is discTap");
+	}
+	
+	static void ballTap(){
+		System.out.println("This is ballTap");
+	}
+	
+	public static void main(String[] args){
+		System.out.println("This is Main Class");
+		pillarTap();
+		mixerTap();
+		washerTap();
+		discTap();
+		ballTap();
+	}
+}
