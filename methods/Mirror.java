c96aca1a365621b7bdd80c51817e10d7fa0634d9
deepthins08 <deepthins.xworkz@gmail.com>
2023-10-28 16:56:13 +0530

    methods

---
+++ methods/Mirror.java
@@ -0,0 +1,31 @@
+class Mirror{
+	
+	static void reflection(){
+		System.out.println("This is reflection");
+	}
+	
+	static void shining(){
+		System.out.println("This is shining");
+	}
+	
+	static void showing(){
+		System.out.println("This is showing");
+	}
+	
+	static void stable(){
+		System.out.println("This is stable");
+	}
+	
+	static void breaking(){
+		System.out.println("This is breaking");
+	}
+	
+	public static void main(String[] args){
+		System.out.println("This is Main Class");
+		reflection();
+		shining();
+		showing();
+		stable();
+		breaking();
+	}
+}
