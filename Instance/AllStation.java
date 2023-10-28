5d875cad8d4391e53457f6de3839a3452c9bff85
deepthins08 <deepthins.xworkz@gmail.com>
2023-10-28 17:18:45 +0530

    Instance

---
+++ Instance/AllStation.java
@@ -0,0 +1,15 @@
+class AllStation{
+	
+	String stationName;
+	String stationType;
+	int noOfStations;
+	double stationArea;
+	String distance;
+	
+	
+	AllStation(){
+		
+		System.out.println("AllStation is created");
+		
+	}
+}
