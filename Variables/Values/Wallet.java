dbe7d784dd1e0a97184aa12105fb789cd1c4a695
deepthins08 <deepthins.xworkz@gmail.com>
2023-10-28 16:44:53 +0530

    variables

---
+++ Variables/Values/Wallet.java
@@ -0,0 +1,20 @@
+class Wallet{
+	
+	static String material="leather";
+	static double cost=299.0;
+	static double width=12.0;
+	static double height=4.0;
+	static double weight=200.0;
+	
+	static void walletInfo(){
+		
+		System.out.println("Information of Wallet is starting");
+		System.out.println("Wallet Details");
+		System.out.println("Wallet Material: "+material);
+		System.out.println("Wallet Cost is: "+cost);
+		System.out.println("Wallet width is "+width);
+		System.out.println("Wallet height is "+height);
+		System.out.println("Wallet weight : "+weight);
+		System.out.println("Information of Wallet is ending");
+	}
+}
