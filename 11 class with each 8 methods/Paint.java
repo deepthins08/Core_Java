0accca80cede706e51fa8cc342e411be3cf568f0
deepthins08 <deepthins.xworkz@gmail.com>
2023-10-28 15:38:48 +0530

    Deepthi is main class

---
+++ 11 class with each 8 methods/Paint.java
@@ -0,0 +1,42 @@
+class Paint{
+	
+	static void brush(){
+		
+		System.out.println("paint brush");
+	}
+	
+	static void coloring(){
+		
+		System.out.println("coloring the paint");
+	}
+	
+	static void designing(){
+		
+		System.out.println("designing the paint");
+	}
+	
+	static void drawing(){
+		
+		System.out.println("drawing the paint");
+	}
+	
+	static void outLine(){
+		
+		System.out.println("Outlining the paint");
+	}
+	
+	static void reshape(){
+		
+		System.out.println("reshaping the Paint");
+	}
+	
+	static void erasing(){
+		
+		System.out.println("erasing the paint");
+	}
+	
+	static void selling(){
+		
+		System.out.println("selling the paint");
+	}
+}
