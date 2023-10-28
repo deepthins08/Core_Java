e0ee61216f01d1158e52164e0072e77c5fb5561d
deepthins08 <deepthins.xworkz@gmail.com>
2023-10-28 16:25:37 +0530

    Opener is the main class

---
+++ Variables/Example2/Laptop.java
@@ -0,0 +1,18 @@
+class Laptop{
+	
+	static String brand;
+	static double cost;
+	static double ram;
+	static double proccessor;
+	static double storage;
+	
+	static void lapInfo(){
+		
+		System.out.println("Laptop specification");
+		System.out.println("Laptop brand is: "+brand);
+		System.out.println("Laptop Cost is: "+cost);
+		System.out.println("Laptop RAM is "+ram);
+		System.out.println("Laptop Proccesor is "+proccessor);
+		System.out.println("Laptop Storage is: "+storage);
+	}
+}
