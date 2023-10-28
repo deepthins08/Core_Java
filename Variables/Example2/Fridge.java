ace0ed6f7cfcd51a4f639ad192a26f675cecae06
deepthins08 <deepthins.xworkz@gmail.com>
2023-10-28 16:24:25 +0530

    Opener is the main class

---
+++ Variables/Example2/Fridge.java
@@ -0,0 +1,19 @@
+class Fridge{
+	
+	
+	static String brand;
+	static double cost;
+	static String type;
+	static String color;
+	static double capacity;
+	
+	static void fridgeInfo(){
+		
+		System.out.println("Fridge specification");
+		System.out.println("Fridge brand is: "+brand);
+		System.out.println("Fridge Cost is: "+cost);
+		System.out.println("Fridge type is "+type);
+		System.out.println("Fridge color is "+color);
+		System.out.println("Fridge capacity is: "+capacity);
+	}
+}
