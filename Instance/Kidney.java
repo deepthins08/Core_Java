44ed2761a00950bfc05bdf06fd1c34038e2cdd67
deepthins08 <deepthins.xworkz@gmail.com>
2023-10-28 17:20:16 +0530

    Instance

---
+++ Instance/Kidney.java
@@ -0,0 +1,14 @@
+class Kidney{
+	
+	String organName;
+	String bloodType;
+	int noOfKidenys;
+	boolean anyDisease;
+	int noOfHealthyKidneys;
+	
+	Kidney(){
+		
+		System.out.println("Kidney is created");
+	}
+
+}
