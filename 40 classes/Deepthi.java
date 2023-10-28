9fd190f403c4d74ea5f2237f2c3bef5f93665962
deepthins08 <deepthins.xworkz@gmail.com>
2023-10-28 17:06:01 +0530

    Main class

---
+++ 40 classes/Deepthi.java
@@ -0,0 +1,49 @@
+class Deepthi{
+
+
+public static void main(String[] wear){
+		
+		Jewellary jewllary;
+		Metal metal;
+		Device device;
+		Vegetables veg;
+		College college;
+		Food food;
+		Bag bag;
+		Ice ice;
+		Water water;
+		Table table;
+		Cup cup;
+		Jockey jockey;
+		Switch switch1;
+		Building building;
+		Car car;
+		Bike bike;
+		Road road;
+		Ubdt ubdt;
+		Wire wire;
+		Ring ring;
+		Fruit fruit;
+		Juice juice;
+		Mat mat;
+		Door door;
+		Color color;
+		Fan fan;
+		Bangle bangle;
+		Hair hair;
+		Screen screen;
+		Cable cable;
+		Sky sky;
+		Tv tv;
+		Mobile mobile;
+		Cloth cloth;
+		Pen pen;
+		Chair chair;
+		Watch watch;
+		Bottle bottle;
+		Camera camera;
+		LapTop laptop;
+		Rain rain;
+		
+	}
+}
