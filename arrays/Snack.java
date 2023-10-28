6408e00150be6bafcfe494df31170c5fb96b53e3
deepthins08 <deepthins.xworkz@gmail.com>
2023-10-28 17:17:52 +0530

    Arrays

---
+++ arrays/Snack.java
@@ -0,0 +1,27 @@
+class Snack{
+	
+	
+	static double costInRupee;
+	static String name;
+	static double quantity;
+	
+	static void setCostInRupee(double costInRupee){
+		
+		Snack.costInRupee=costInRupee;
+		
+	}
+	
+	static void setName(double name){
+		
+		Snack.name=name;
+		
+	}
+	
+	static void setQuantity(double quantity){
+		
+		Snack.quantity=quantity;
+		
+	}
+	
+	
+}
