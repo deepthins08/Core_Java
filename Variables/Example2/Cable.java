ece8413beb940868c8b97101a17024a162abb4fe
deepthins08 <deepthins.xworkz@gmail.com>
2023-10-28 16:22:04 +0530

    Opener is the main class

---
+++ Variables/Example2/Cable.java
@@ -0,0 +1,18 @@
+class Cable{
+	
+	static String brand;
+	static double cost;
+	static double length;
+	static String quality;
+	static String type;
+	
+	static void cableInfo(){
+		
+		System.out.println("Cable specification");
+		System.out.println("Cable brand is: "+brand);
+		System.out.println("Cable Cost is: "+cost);
+		System.out.println("Cable length is "+length);
+		System.out.println("Cable quality is "+quality);
+		System.out.println("Cable type is: "+type);
+	}
+}
