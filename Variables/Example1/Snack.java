c777654f3b4bf6f6a13d60133563a15994cf65b1
deepthins08 <deepthins.xworkz@gmail.com>
2023-10-28 16:10:45 +0530

    Starter is a main class

---
+++ Variables/Example1/Snack.java
@@ -0,0 +1,27 @@
+class Snack{
+	
+	
+	static double costInRupee;
+	static String name;
+	static double quantity;
+	
+	static void setCostInRupee(double costInRupee){
+		
+		Snack.costInRupee=costInRupee;
+		
+	}
+	
+	static void setName(String name){
+		
+		Snack.name=name;
+		
+	}
+	
+	static void setQuantity(double quantity){
+		
+		Snack.quantity=quantity;
+		
+	}
+	
+	
+}
