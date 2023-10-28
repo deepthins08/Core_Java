aaa76441317feab19d22bd20f37e7674255af0e1
deepthins08 <deepthins.xworkz@gmail.com>
2023-10-28 16:23:57 +0530

    Opener is the main class

---
+++ Variables/Example2/Door.java
@@ -0,0 +1,19 @@
+class Door{
+	
+	
+	static double height;
+	static double cost;
+	static String type;
+	static String color;
+	static double weight;
+	
+	static void doorInfo(){
+		
+		System.out.println("Door specification");
+		System.out.println("Door height is:" +height);
+		System.out.println("Door Cost is: "+cost);
+		System.out.println("Door type is "+type);
+		System.out.println("Door color is "+color);
+		System.out.println("Door weight is: "+weight);
+	}
+}
