e769beee1bb413988f19ca52bcea9d5f54861096
deepthins08 <deepthins.xworkz@gmail.com>
2023-10-28 15:36:20 +0530

    Deepthi is main class

---
+++ 11 class with each 8 methods/Fruits.java
@@ -0,0 +1,42 @@
+class Fruits{
+	
+	static void orange(){
+		
+		System.out.println("orange");
+	}
+	
+	static void apple(){
+		
+		System.out.println("apple");
+	}
+	
+	static void pineApple(){
+		
+		System.out.println("pineAplle");
+	}
+	
+	static void dragonFruit(){
+		
+		System.out.println("dragonFruit");
+	}
+	
+	static void grapes(){
+		
+		System.out.println("grapes");
+	}
+	
+	static void avacado(){
+		
+		System.out.println("avacado");
+	}
+	
+	static void strawBerry(){
+		
+		System.out.println("strawBerry");
+	}
+	
+	static void blackBerry(){
+		
+		System.out.println("blackBerry");
+	}
+}
