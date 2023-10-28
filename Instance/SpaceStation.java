041cba8bd6cc6ac70b75cf33e2c5467e2aa1860f
deepthins08 <deepthins.xworkz@gmail.com>
2023-10-28 17:21:46 +0530

    Instance

---
+++ Instance/SpaceStation.java
@@ -0,0 +1,13 @@
+class SpaceStation{
+	
+	String spaceStationName;
+	String stationDircetion;
+	int noOfSpaceStation;
+	double distanceFromEarth;
+	int noOfCountiresHavingSpaceStation;
+	
+	SpaceStation(){
+		
+		System.out.println("SpaceStation is created");
+	}
+}
