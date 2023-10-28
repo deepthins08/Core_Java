374c537463224234acf43b4696373788bb8700c1
deepthins08 <deepthins.xworkz@gmail.com>
2023-10-28 16:44:14 +0530

    variables

---
+++ Variables/Values/Slipper.java
@@ -0,0 +1,20 @@
+class Slipper{
+	
+	static String brand="Bata";
+	static String color="black";
+	static String pattern="shoes";
+	static double size=8;
+	static double cost=799.0;
+	
+	static void slipperInfo(){
+		
+		System.out.println("Information of Slipper is starting");
+		System.out.println("Slipper Details");
+		System.out.println("Slipper size: "+brand);
+		System.out.println("Slipper Cost is: "+color);
+		System.out.println("Slipper color is "+pattern);
+		System.out.println("Slipper brand is "+size);
+		System.out.println("Slipper type : "+cost);
+		System.out.println("Information of Slipper is ending");
+	}
+}
