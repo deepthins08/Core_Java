27b2a53ec96ecf5631b420c614894804ec187a11
deepthins08 <deepthins.xworkz@gmail.com>
2023-10-28 16:26:41 +0530

    This is the main class

---
+++ Variables/Example2/Opener.java
@@ -0,0 +1,45 @@
+class Opener{
+	
+	public static void main(String[] variables){
+		
+		System.out.println("This is main method");
+		System.out.println("Displaying the Mobile Specifications...... \n");
+		Mobile.specInfo();
+		System.out.println("\n");
+		
+		System.out.println("Displaying the Laptop Specifications...... \n");
+		Laptop.lapInfo();
+		System.out.println("\n");
+		
+		System.out.println("Displaying the Charger Specifications...... \n");
+		Charger.chargerInfo();
+		System.out.println("\n");
+		
+		System.out.println("Displaying the Cable Specifications...... \n");
+		Cable.cableInfo();
+		System.out.println("\n");
+		
+		System.out.println("Displaying the Pendrive Specifications...... \n");
+		Pendrive.penDriveInfo();
+		System.out.println("\n");
+		
+		System.out.println("Displaying the Fridge Specifications...... \n");
+		Fridge.fridgeInfo();
+		
+		System.out.println("\n");
+		System.out.println("Displaying the Door Specifications...... \n");
+		Door.doorInfo();
+		
+		System.out.println("\n");
+		System.out.println("Displaying the Fruit Specs...... \n");
+		Fruit.fruitInfo();
+		
+		System.out.println("\n");
+		System.out.println("Displaying the paper Specs...... \n");
+		Paper.paperInfo();
+		
+		System.out.println("\n");
+		System.out.println("Displaying the Bottle Specs...... \n");
+		Bottle.bottleInfo();
+	}
+}
