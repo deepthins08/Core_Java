eed43bda7287198a0fa63ab46c7ce3cdf870b535
deepthins08 <deepthins.xworkz@gmail.com>
2023-10-28 16:27:06 +0530

    Opener is the main class

---
+++ Variables/Example2/Paper.java
@@ -0,0 +1,18 @@
+class Paper{
+	
+	static String color;
+	static double cost;
+	static String type;
+	static String shape;
+	static double weight;
+	
+	static void paperInfo(){
+		
+		System.out.println("Paper specification");
+		System.out.println("Paper color is: "+color);
+		System.out.println("Paper Cost is: "+cost);
+		System.out.println("Paper type is "+type);
+		System.out.println("Paper shape is "+shape);
+		System.out.println("Paper weight is: "+weight);
+	}
+}
