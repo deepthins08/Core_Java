9b58a0791d53783440cfd745c1fcdf7cc8f92d5e
deepthins08 <deepthins.xworkz@gmail.com>
2023-10-28 15:39:14 +0530

    Deepthi is main class

---
+++ 11 class with each 8 methods/Street.java
@@ -0,0 +1,42 @@
+class Street{
+	
+	static void road(){
+		
+		System.out.println("street have a road");
+	}
+	
+	static void bike(){
+		
+		System.out.println("bike is standing on the street");
+	}
+	
+	static void car(){
+		
+		System.out.println("car is going in this street");
+	}
+	
+	static void cycle(){
+		
+		System.out.println("cycle is stolen");
+	}
+	
+	static void people(){
+		
+		System.out.println("people are walking around");
+	}
+	
+	static void van(){
+		
+		System.out.println("school van is coming");
+	}
+	
+	static void bus(){
+		
+		System.out.println("poeple are taken the bus to travel");
+	}
+	
+	static void scooty(){
+		
+		System.out.println("i have a scooty");
+	}
+}
