b322984525a1e6d5feaa80bea318a9bc519b81dc
deepthins08 <deepthins.xworkz@gmail.com>
2023-10-28 16:09:23 +0530

    Starter is a main class

---
+++ Variables/Example1/FootPath.java
@@ -0,0 +1,21 @@
+class FootPath{
+	
+	static double noOfPedestrians;
+	static String footPathName;
+	static double noOfLightPole;
+	
+	static void setNoOfPedestrians(double noOfPedestrians){
+		
+		FootPath.noOfPedestrians=noOfPedestrians;
+	}
+	
+	static void setFootPathName(String footPathName){
+		
+		FootPath.footPathName=footPathName;
+	}
+	
+	static void setNoOfLightPole(double noOfLightPole){
+		
+		FootPath.noOfLightPole=noOfLightPole;
+	}
+}
