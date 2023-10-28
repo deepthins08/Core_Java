a675b99a77a260bc8a05548d0eb5f2c2d8707082
deepthins08 <deepthins.xworkz@gmail.com>
2023-10-28 15:35:21 +0530

    Deepthi is main class

---
+++ 11 class with each 8 methods/Car.java
@@ -0,0 +1,42 @@
+class Car{
+	
+	static void seat(){
+		
+		System.out.println("car seat");
+	}
+	
+	static void belt(){
+		
+		System.out.println("seat belt");
+	}
+	
+	static void tyre(){
+		
+		System.out.println("car tyres");
+	}
+	
+	static void steering(){
+		
+		System.out.println("car steering");
+	}
+	
+	static void gear(){
+		
+		System.out.println("car gear");
+	}
+	
+	static void ac(){
+		
+		System.out.println("air condition");
+	}
+	
+	static void roofTop(){
+		
+		System.out.println("car roofTop");
+	}
+	
+	static void engine(){
+		
+		System.out.println("car engine");
+	}
+}
