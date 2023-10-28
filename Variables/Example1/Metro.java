f184187c7008372e8111f923bd20302dd5cd0e70
deepthins08 <deepthins.xworkz@gmail.com>
2023-10-28 16:09:43 +0530

    Starter is a main class

---
+++ Variables/Example1/Metro.java
@@ -0,0 +1,21 @@
+class Metro{
+	
+	static String stationName;
+	static String doorOpeningDricetion;
+	static int platform;
+	
+	static void setStationName(String stationName){
+		
+		Metro.stationName=stationName;
+	}
+	
+	static void setDoorOpeningDirection(String doorOpeningDricetion){
+		
+		Metro.doorOpeningDricetion=doorOpeningDricetion;
+	}
+	
+	static void setPlatform(int platform){
+		
+		Metro.platform=platform;
+	}
+}
