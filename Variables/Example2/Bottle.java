74f500aa1455c0c7de6123296e2de7ddef419c69
deepthins08 <deepthins.xworkz@gmail.com>
2023-10-28 16:19:21 +0530

    Opener is the main class

---
+++ Variables/Example2/Bottle.java
@@ -0,0 +1,18 @@
+class Bottle{
+	
+	static String color;
+	static double cost;
+	static String type;
+	static String shape;
+	static double weight;
+	
+	static void bottleInfo(){
+		
+		System.out.println("Bottle specification");
+		System.out.println("Bottle color is: "+color);
+		System.out.println("Bottle Cost is: "+cost);
+		System.out.println("Bottle type is "+type);
+		System.out.println("Bottle shape is "+shape);
+		System.out.println("Bottle weight is: "+weight);
+	}
+}
