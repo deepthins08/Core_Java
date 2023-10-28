88123a8b50ea154291e73c57a4db5c4dfa5cfed6
deepthins08 <deepthins.xworkz@gmail.com>
2023-10-28 17:17:23 +0530

    Arrays

---
+++ arrays/FrequencyStarter.java
@@ -0,0 +1,14 @@
+class FrequencyStarter{
+	
+	public static void main(String[] args){
+	
+	int freq[]={2,3,2,6,3,5};
+	int value=2;
+	
+	
+	System.out.println("Frequency of "+value+ " is "+Frequency.show(freq,value));
+	
+	value=6;
+	System.out.println("Frequency of "+value+ " is "+Frequency.show(freq,value));
+}
+}
