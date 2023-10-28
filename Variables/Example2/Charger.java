8f96e7c2509ab496416f5ba44fadaa91fd00598e
deepthins08 <deepthins.xworkz@gmail.com>
2023-10-28 16:22:39 +0530

    Opener is the main class

---
+++ Variables/Example2/Charger.java
@@ -0,0 +1,18 @@
+class Charger{
+	
+	static String brand;
+	static double cost;
+	static String color;
+	static double voltage;
+	static String madein;
+	
+	static void chargerInfo(){
+		
+		System.out.println("Charger specification");
+		System.out.println("charger brand is: "+brand);
+		System.out.println("charger Cost is: "+cost);
+		System.out.println("charger color is "+color);
+		System.out.println("charger Voltage is "+voltage);
+		System.out.println("charger is made in : "+madein);
+	}
+}
