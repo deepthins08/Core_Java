9fe62bd84b87fcac5f8ddb2ae4d8dbfb8eaa089f
deepthins08 <deepthins.xworkz@gmail.com>
2023-10-28 15:37:06 +0530

    Deepthi is main class

---
+++ 11 class with each 8 methods/Key.java
@@ -0,0 +1,34 @@
+class Key{
+	
+	static void keyOpen(){
+		System.out.println("this opening key");
+	}
+	
+	static void keyClose(){
+		System.out.println("Closing Key");
+	}
+	
+	static void keyRust(){
+		System.out.println("Rusted Key");
+	}
+	
+	static void keyLost(){
+		System.out.println("Losted Key");
+	}
+	
+	static void keyFound(){
+		System.out.println("Founded Key");
+	}
+	
+	static void keyThrow(){
+		System.out.println("Throwed Key");
+	}
+	
+	static void keyBought(){
+		System.out.println("Boughted Key");
+	}
+	
+	static void keyStarter(){
+		System.out.println("Keystarter key");
+	}
+}
