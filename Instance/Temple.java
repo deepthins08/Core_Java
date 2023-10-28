0be8a82bfef60a6233ae8d73e044c298f1bc58ef
deepthins08 <deepthins.xworkz@gmail.com>
2023-10-28 17:22:04 +0530

    Instance

---
+++ Instance/Temple.java
@@ -0,0 +1,15 @@
+class Temple{
+	
+	String templeName;
+	String godName;
+	double templeArea;
+	double templeHieght;
+	String templeLocation;
+	
+	
+	Temple(){
+		
+		System.out.println("Temple is created");
+	}
+
+}
