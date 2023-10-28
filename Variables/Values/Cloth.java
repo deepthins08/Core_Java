205ad019d5cfea60355dcfcc90ebac2db407255f
deepthins08 <deepthins.xworkz@gmail.com>
2023-10-28 16:39:25 +0530

    variables

---
+++ Variables/Values/Cloth.java
@@ -0,0 +1,20 @@
+class Cloth{
+	
+	static String size="L";
+	static double cost=150.0;
+	static String color="gray";
+	static String brand="Puma";
+	static String type="Silk";
+	
+	static void clothInfo(){
+		
+		System.out.println("Information of Cloth is starting");
+		System.out.println("Cloth Details");
+		System.out.println("Cloth size: "+size);
+		System.out.println("Cloth Cost is: "+cost);
+		System.out.println("Cloth color is "+color);
+		System.out.println("Cloth brand is "+brand);
+		System.out.println("Cloth type : "+type);
+		System.out.println("Information of Cloth is ending");
+	}
+}
