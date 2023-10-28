e75babf66a0baa95c600bfa374f138d7c477c032
deepthins08 <deepthins.xworkz@gmail.com>
2023-10-28 16:12:33 +0530

    Starter is a main class

---
+++ Variables/Example1/Tree.java
@@ -0,0 +1,22 @@
+class Tree{
+	
+	static double heightInFeet;
+	static double noOfTrees;
+	static String treeName;
+	
+	static void setHeightInFeet(double heightInFeet){
+		
+		Tree.heightInFeet=heightInFeet;
+	}
+	
+	static void setNoOfTrees(double noOfTrees){
+		
+		Tree.noOfTrees=noOfTrees;
+	}
+	
+	static void setTreeName(String treeName){
+		
+		Tree.treeName=treeName;
+	}
+	
+}
