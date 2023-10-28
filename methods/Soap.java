cdc7065b8188fd1e2cc991c9a659745c89ae74d9
deepthins08 <deepthins.xworkz@gmail.com>
2023-10-28 16:57:00 +0530

    methods

---
+++ methods/Soap.java
@@ -0,0 +1,31 @@
+class Soap{
+	
+	static void babySoap(){
+		System.out.println("This is babySoap");
+	}
+	
+	static void luxSoap(){
+		System.out.println("This is luxSoap");
+	}
+	
+	static void santoorSoap(){
+		System.out.println("This is santoorSoap");
+	}
+	
+	static void mysoreSandalSoap(){
+		System.out.println("This is mysoreSandalSoap");
+	}
+	
+	static void rexonaSoap(){
+		System.out.println("This is rexonaSoap");
+	}
+	
+	public static void main(String[] args){
+		System.out.println("This is Main Class");
+		babySoap();
+		luxSoap();
+		santoorSoap();
+		mysoreSandalSoap();
+		rexonaSoap();
+	}
+}
