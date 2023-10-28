fe3866d379307ae249e803ce1f02da6b7a48ca3b
deepthins08 <deepthins.xworkz@gmail.com>
2023-10-28 16:40:56 +0530

    variables

---
+++ Variables/Values/Jar.java
@@ -0,0 +1,21 @@
+class Jar{
+	
+	static String color="yeloow";
+	static String type="ceramic";
+	static double weight=5;
+	static double height=2;
+	static double cost=199;
+	
+	
+	static void jarInfo(){
+		
+		System.out.println("Information of Jar is starting");
+		System.out.println("Jar Details");
+		System.out.println("Jar color : "+color);
+		System.out.println("Jar type is: "+type);
+		System.out.println("Jar weight: "+weight);
+		System.out.println("Jar height is "+height);
+		System.out.println("Jar cost is "+cost);
+		System.out.println("Information of Jar is ending");
+	}
+}
