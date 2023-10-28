389492ad7f53ede1e20c126f058e420c3d52bead
deepthins08 <deepthins.xworkz@gmail.com>
2023-10-28 16:10:06 +0530

    Starter is a main class

---
+++ Variables/Example1/Road.java
@@ -0,0 +1,22 @@
+class Road{
+	
+	static String roadName;
+	static String vehicleName;
+	static String roadType;
+	
+	static void setRoadName(String roadName){
+		
+		Road.roadName=roadName;
+	}
+	
+	static void setVehicleName(String vehicleName){
+		
+		Road.vehicleName=vehicleName;
+	}
+	
+	static void setRoadType(String roadType){
+		
+		Road.roadType=roadType;
+	}
+	
+}
