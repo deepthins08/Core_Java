f6813466acbe3fbe7c69a4ce9312cd0272f7dd8a
deepthins08 <deepthins.xworkz@gmail.com>
2023-10-28 15:28:53 +0530

    Deepthi is main class

---
+++ 11 class with each 8 methods/Building.java
@@ -0,0 +1,47 @@
+class Building{
+	
+	static void room(){
+		
+		System.out.println("Building conatain rooms");
+	}
+	
+	static void stairs(){
+		
+		System.out.println("it has stairs");
+	}
+	
+	static void lift(){
+		
+		System.out.println("it has lift");
+	}
+	
+	static void corridor(){
+		
+		System.out.println("every floor has corridor");
+	}
+	
+	static void balcany(){
+		
+		System.out.println("balcany is beautiful");
+	}
+	
+	static void groundFloor(){
+		
+		System.out.println("parking is allocated in groundFloor");
+	}
+	
+	static void RoofTop(){
+		
+		System.out.println("Rooftop is the top of the building");
+	}
+	
+	static void wall(){
+		
+		System.out.println("wall of the building");
+	}
+	
+	static void backYard(){
+		
+		System.out.println("backyard of the building");
+	}
+}
