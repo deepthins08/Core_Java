305e4640b4119c1352109b2e7354aa1019c8d46e
deepthins08 <deepthins.xworkz@gmail.com>
2023-10-28 16:24:50 +0530

    Opener is the main class

---
+++ Variables/Example2/Fruit.java
@@ -0,0 +1,18 @@
+class Fruit{
+	
+	static String color;
+	static double cost;
+	static String type;
+	static String shape;
+	static double weight;
+	
+	static void fruitInfo(){
+		
+		System.out.println("Fruit specification");
+		System.out.println("Fruit color is: "+color);
+		System.out.println("Fruit Cost is: "+cost);
+		System.out.println("Fruit type is "+type);
+		System.out.println("Fruit shape is "+shape);
+		System.out.println("Fruit weight is: "+weight);
+	}
+}
