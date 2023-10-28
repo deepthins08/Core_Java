0c7ceca78fbd227e15d04a14f01945952293e0ca
deepthins08 <deepthins.xworkz@gmail.com>
2023-10-28 16:43:27 +0530

    variables

---
+++ Variables/Values/Kerchief.java
@@ -0,0 +1,22 @@
+class Kerchief{
+	
+	static double cost=50;
+	static String color="white";
+	static String brand="jockey";
+	static double quantity=2;
+	static String madein="india";
+	
+	
+	
+	static void kerchiefInfo(){
+		
+		System.out.println("Information of Kerchief is starting");
+		System.out.println("Kerchief Details");
+		System.out.println("Kerchief color : "+cost);
+		System.out.println("Kerchief type is: "+color);
+		System.out.println("Kerchief weight: "+brand);
+		System.out.println("Kerchief height is "+quantity);
+		System.out.println("Kerchief cost is "+madein);
+		System.out.println("Information of Kerchief is ending");
+	}
+}
