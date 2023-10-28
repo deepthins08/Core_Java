971a4621eab642e8f8a44eb2b1aba9ec3a0f02f2
deepthins08 <deepthins.xworkz@gmail.com>
2023-10-28 17:19:39 +0530

    Instance

---
+++ Instance/Hospital.java
@@ -0,0 +1,13 @@
+class Hospital{
+	
+	int noOfAmbulance;
+	String hospitalName;
+	String location;
+	int noOfPatients;
+	int noOfBeds;
+	
+	Hospital(){
+		
+		System.out.println("Hospital is created");
+	}
+}
