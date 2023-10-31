15d0074e829862a09d146a6e718f2b2d961f4072
deepthins08 <deepthins.xworkz@gmail.com>
2023-10-31 12:39:10 +0530

    using constructor overloading

---
+++ Instance/ShowRoomStarter.java
@@ -0,0 +1,89 @@
+class ShowRoomStarter{
+	
+	public static void main(String[] showrooms){
+		
+		System.out.println("Starting main in ShowRoomStarter");
+		ShowRoom showroom=new ShowRoom("Hero Honda");
+		System.out.println("showroom name is "+showroom.showRoomName);
+		System.out.println("showroom location is "+showroom.location);
+		System.out.println("showroom noOfVehicles are "+showroom.noOfVehicles);
+		System.out.println("showroom areaInSqFt is "+showroom.areaInSqFt);
+		System.out.println("showroom noOfFloors are "+showroom.noOfFloors);
+		System.out.println("showroom mobile is "+showroom.mobile);
+		System.out.println("showroom ownerName is "+showroom.ownerName);
+		System.out.println("showroom rating is "+showroom.rating);
+		
+		ShowRoom showroom1=new ShowRoom("Hero Honda","Bangalore");
+		System.out.println("showroom1 name is "+showroom1.showRoomName);
+		System.out.println("showroom1 location is "+showroom1.location);
+		System.out.println("showroom1 noOfVehicles are "+showroom1.noOfVehicles);
+		System.out.println("showroom1 areaInSqFt is "+showroom1.areaInSqFt);
+		System.out.println("showroom1 noOfFloors are "+showroom1.noOfFloors);
+		System.out.println("showroom1 mobile is "+showroom1.mobile);
+		System.out.println("showroom1 ownerName is "+showroom1.ownerName);
+		System.out.println("showroom1 rating is "+showroom1.rating);
+		
+		ShowRoom showroom2=new ShowRoom("Hero Honda","Bangalore",100);
+		System.out.println("showroom2 name is "+showroom2.showRoomName);
+		System.out.println("showroom2 location is "+showroom2.location);
+		System.out.println("showroom2 noOfVehicles are "+showroom2.noOfVehicles);
+		System.out.println("showroom2 areaInSqFt is "+showroom2.areaInSqFt);
+		System.out.println("showroom2 noOfFloors are "+showroom2.noOfFloors);
+		System.out.println("showroom2 mobile is "+showroom2.mobile);
+		System.out.println("showroom2 ownerName is "+showroom2.ownerName);
+		System.out.println("showroom2 rating is "+showroom2.rating);
+		
+		ShowRoom showroom3=new ShowRoom("Hero Honda","Bangalore",100,12000);
+		System.out.println("showroom3 name is "+showroom3.showRoomName);
+		System.out.println("showroom3 location is "+showroom3.location);
+		System.out.println("showroom3 noOfVehicles are "+showroom3.noOfVehicles);
+		System.out.println("showroom3 areaInSqFt is "+showroom3.areaInSqFt);
+		System.out.println("showroom3 noOfFloors are "+showroom3.noOfFloors);
+		System.out.println("showroom3 mobile is "+showroom3.mobile);
+		System.out.println("showroom3 ownerName is "+showroom3.ownerName);
+		System.out.println("showroom3 rating is "+showroom3.rating);
+		
+		ShowRoom showroom4=new ShowRoom("Hero Honda","Bangalore",100,12000,4);
+		System.out.println("showroom4 name is "+showroom4.showRoomName);
+		System.out.println("showroom4 location is "+showroom4.location);
+		System.out.println("showroom4 noOfVehicles are "+showroom4.noOfVehicles);
+		System.out.println("showroom4 areaInSqFt is "+showroom4.areaInSqFt);
+		System.out.println("showroom4 noOfFloors are "+showroom4.noOfFloors);
+		System.out.println("showroom4 mobile is "+showroom4.mobile);
+		System.out.println("showroom4 ownerName is "+showroom4.ownerName);
+		System.out.println("showroom4 rating is "+showroom4.rating);
+		
+		ShowRoom showroom5=new ShowRoom("Hero Honda","Bangalore",100,12000,4,7484488483L);
+		System.out.println("showroom5 name is "+showroom5.showRoomName);
+		System.out.println("showroom5 location is "+showroom5.location);
+		System.out.println("showroom5 noOfVehicles are "+showroom5.noOfVehicles);
+		System.out.println("showroom5 areaInSqFt is "+showroom5.areaInSqFt);
+		System.out.println("showroom5 noOfFloors are "+showroom5.noOfFloors);
+		System.out.println("showroom5 mobile is "+showroom5.mobile);
+		System.out.println("showroom5 ownerName is "+showroom5.ownerName);
+		System.out.println("showroom5 rating is "+showroom5.rating);
+		
+		ShowRoom showroom6=new ShowRoom("Hero Honda","Bangalore",100,12000,4,7484488483L,"Mathew");
+		System.out.println("showroom6 name is "+showroom6.showRoomName);
+		System.out.println("showroom6 location is "+showroom6.location);
+		System.out.println("showroom6 noOfVehicles are "+showroom6.noOfVehicles);
+		System.out.println("showroom6 areaInSqFt is "+showroom6.areaInSqFt);
+		System.out.println("showroom6 noOfFloors are "+showroom6.noOfFloors);
+		System.out.println("showroom6 mobile is "+showroom6.mobile);
+		System.out.println("showroom6 ownerName is "+showroom6.ownerName);
+		System.out.println("showroom6 rating is "+showroom6.rating);
+		
+		ShowRoom showroom7=new ShowRoom("Hero Honda","Bangalore",100,12000,4,7484488483L,"Mathew",7);
+		System.out.println("showroom7 name is "+showroom7.showRoomName);
+		System.out.println("showroom7 location is "+showroom7.location);
+		System.out.println("showroom7 noOfVehicles are "+showroom7.noOfVehicles);
+		System.out.println("showroom7 areaInSqFt is "+showroom7.areaInSqFt);
+		System.out.println("showroom7 noOfFloors are "+showroom7.noOfFloors);
+		System.out.println("showroom7 mobile is "+showroom7.mobile);
+		System.out.println("showroom7 ownerName is "+showroom7.ownerName);
+		System.out.println("showroom7 rating is "+showroom7.rating);
+		
+		System.out.println("Starting main in ShowRoomStarter");
+		
+	}
+}
