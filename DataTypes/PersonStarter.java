8876afeef1c96fdbf2212dea9892be19798ab531
deepthins08 <deepthins.xworkz@gmail.com>
2023-11-08 20:00:14 +0530

    Array in datatypes

--- DataTypes/PersonStarter.java
+++ DataTypes/PersonStarter.java
@@ -5,29 +5,20 @@ class PersonStarter{
 		
 		System.out.println("Starting main in PersonStarter");
 		
-		/***Domain domain=Domain.GMAIL;
-		Email[] email=new Email(domain,"anthoony@gmail.com","Anthony","Duke");
-		Person person=new Person("Anthony",email);
-		person.name="anthony";
-		Person person1=new Person("John",email);
-		person1.email="johndepp@gmail.com";
-		
-		Person person2=new Person("Demon",email);
-		person2.email="demonsalvetor@gmail";
-		
-		Email[] emailArray={person,person1,person2};***/
 		Domain domain=Domain.GMAIL;
-		//Email email=new Email(domain,"anthoony@gmail","Anthony","Duke");
-		Email[] emailArray=new Email[3];
-		System.out.println(emailArray[0]=email);
 		
-		Email email=new Email(domain,"anthoony@gmail","Anthony","Duke");
+		Email email=new Email();
+		email.setEmail(domain,"stephen@gmail.com","Stephen","Salvator");
 		email.showInfo();
-		Person person=new Person("Anthony",emailArray);
+		String[] mail=new String[3];
+		mail[0]="stephen@gmail";
+		mail[1]="damon@gmail.com";
+		mail[2]="taylor@gmail.com";
+		System.out.println(mail);
+		Person person=new Person();
+		person.setMail(mail);
+		person.name="Klevin";
 		person.showInfo();
-		
-		
-		
 		
 		System.out.println("Ending main in PersonStarter");
 	}
