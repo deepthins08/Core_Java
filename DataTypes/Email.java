7fee85e75f93c98f8bc1163b3d37ecb5c37bdd69
deepthins08 <deepthins.xworkz@gmail.com>
2023-11-08 12:13:14 +0530

    Array in datatypes

---
+++ DataTypes/Email.java
@@ -0,0 +1,32 @@
+class Email{
+	
+	Domain domain;
+	String id;
+	String fname;
+	String lastName;
+	
+	Email(Domain domain,String id,String fname,String lastName){
+		
+		this.domain=domain;
+		this.id=id;
+		this.fname=fname;
+		this.lastName=lastName;
+		System.out.println("Created Domain,int,String,String with argument constructor");
+	}
+	
+	void showInfo(){
+		
+		System.out.println("Starting showInfo in Email");
+		System.out.println("Email id is "+this.id);
+		System.out.println("Email fname is "+this.fname);
+		System.out.println("Email lastName is "+this.lastName);
+		System.out.println("Email domain is "+this.domain);
+		
+		if(domain!=null){
+			
+			System.out.println("Email domain is "+this.domain);
+		}
+		
+		System.out.println("Ending showInfo in Email");
+	}
+}
