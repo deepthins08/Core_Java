cc6b2ab40e784aae4d8afcb2970505af28ce190b
deepthins08 <deepthins.xworkz@gmail.com>
2023-11-17 18:50:04 +0530

    TypeCaste

---
+++ Inheritance/TypeCasting/Generator.java
@@ -0,0 +1,12 @@
+class Generator extends Machine{
+	
+	Generator(){
+		
+		System.out.println("Created non-arg const for Generator");
+	}
+	
+	void powerSupply(){
+		
+		System.out.println("Displaying powerSupply in Generator");
+	}
+}
