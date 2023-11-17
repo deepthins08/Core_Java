a6e696f89ff0eb204adbd7b0a7fef6b330a48aa4
deepthins08 <deepthins.xworkz@gmail.com>
2023-11-17 18:51:12 +0530

    TypeCaste

---
+++ Inheritance/TypeCasting/Food.java
@@ -0,0 +1,12 @@
+class Food extends Plant{
+	
+	Food(){
+		
+		System.out.println("Created no-arg const for Food");
+	}
+	
+	void flavour(){
+		
+		System.out.println("Displaying flavour in Food");
+	}
+}
