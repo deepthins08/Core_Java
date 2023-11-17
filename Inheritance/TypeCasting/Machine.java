299bacc0ff2b7b059b92e544b44ae5afac693c32
deepthins08 <deepthins.xworkz@gmail.com>
2023-11-17 18:49:49 +0530

    TypeCaste

---
+++ Inheritance/TypeCasting/Machine.java
@@ -0,0 +1,12 @@
+class Machine{
+	
+	Machine(){
+		
+		System.out.println("Created no-arg const for Machine");
+	}
+	
+	void start(){
+		
+		System.out.println("Displaying start in Machine");
+	}
+}
