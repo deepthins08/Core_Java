ec6ed438b7895caa8bd6fbd2b14383b466d50b6e
deepthins08 <deepthins.xworkz@gmail.com>
2023-11-20 15:26:34 +0530

    TypeCaste

---
+++ Inheritance/TypeCasting/ProductOwner.java
@@ -0,0 +1,13 @@
+class ProductOwner extends Person{
+	
+	ProductOwner(){
+		
+		
+		System.out.println("Created no-arg const in ProductOwner");
+	}
+	
+	void validate(){
+		
+		System.out.println("Displaying validate in ProductOwner");
+	}
+}
