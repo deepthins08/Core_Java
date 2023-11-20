89c801b3de31e291e2d396c6ac3bf9b27595b70b
deepthins08 <deepthins.xworkz@gmail.com>
2023-11-20 15:25:25 +0530

    TypeCaste

---
+++ Inheritance/TypeCasting/Developer.java
@@ -0,0 +1,12 @@
+class Developer extends Person{
+	
+	Developer(){
+		
+		System.out.println("Created no arg const in Developer");
+	}
+	
+	void code(){
+		
+		System.out.println("Displaying code in developer");
+	}
+}
