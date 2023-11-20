9677df672d1f6efb111f176d4e2193ed830b6bac
deepthins08 <deepthins.xworkz@gmail.com>
2023-11-20 15:26:13 +0530

    TypeCaste

---
+++ Inheritance/TypeCasting/BusinessAnalyst.java
@@ -0,0 +1,12 @@
+class BusinessAnalyst extends Person{
+	
+	BusinessAnalyst(){
+		
+		System.out.println("Created no-arg const in BusinessAnalyst");
+	}
+	
+	void checkRequirement(){
+		
+		System.out.println("Displaying checkRequirement in BusinessAnalyst");
+	}
+}
