902e36af161f417df27896c6b0802cf393eb2858
deepthins08 <deepthins.xworkz@gmail.com>
2023-11-20 15:25:47 +0530

    TypeCaste

---
+++ Inheritance/TypeCasting/Tester.java
@@ -0,0 +1,12 @@
+class Tester extends Developer{
+	
+	Tester(){
+		
+		System.out.println("Created no arg const in tester");
+	}
+	
+	void qualityCheck(){
+		
+		System.out.println("Displaying qualityCheck in Tester");
+	}
+}
