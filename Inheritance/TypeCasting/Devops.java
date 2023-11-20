7b0b2f752bca946093074714fcd35c77a13449b9
deepthins08 <deepthins.xworkz@gmail.com>
2023-11-20 15:27:10 +0530

    TypeCaste

---
+++ Inheritance/TypeCasting/Devops.java
@@ -0,0 +1,12 @@
+class Devops extends Person{
+	
+	Devops(){
+		
+		System.out.println("Created no arg in Devops");
+	}
+	
+	void runBuild(){
+		
+		System.out.println("Displaying runBuild in Devops");
+	}
+}
