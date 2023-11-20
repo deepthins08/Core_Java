8335f167446c21e378a6ea3671621f409ee3dd2c
deepthins08 <deepthins.xworkz@gmail.com>
2023-11-20 15:26:53 +0530

    TypeCaste

---
+++ Inheritance/TypeCasting/DataBaseDeveloper.java
@@ -0,0 +1,12 @@
+class DataBaseDeveloper extends Developer{
+	
+	DataBaseDeveloper(){
+		
+		System.out.println("Created no arg const in DataBaseDeveloper");
+	}
+	
+	void runScripts(){
+		
+		System.out.println("Displaying runScripts in DataBaseDeveloper");
+	}
+}
