2a6abdbba48b86fffacfc3873cb3f358fa4044d6
deepthins08 <deepthins.xworkz@gmail.com>
2023-11-21 20:34:30 +0530

    Chaining

---
+++ Inheritance/Chaining/GorillaGlass.java
@@ -0,0 +1,8 @@
+class GorillaGlass extends LaminatedGlass{
+	
+	GorillaGlass(double length,boolean thickness){
+		
+		super(length,thickness);
+		System.out.println("double,boolean arg const in GorillaGlass");
+	}
+}
