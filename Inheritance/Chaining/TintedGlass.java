75530a4ebea555649388803210d82fa963b5b5de
deepthins08 <deepthins.xworkz@gmail.com>
2023-11-21 20:36:15 +0530

    Chaining

---
+++ Inheritance/Chaining/TintedGlass.java
@@ -0,0 +1,8 @@
+class TintedGlass extends ToughenedGlass{
+	
+	TintedGlass(double length,boolean thickness){
+		
+		super(length,thickness);
+		System.out.println("double,boolean arg const in TintedGlass");
+	}
+}
