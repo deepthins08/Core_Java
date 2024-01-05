b71d42027c24305c2f3722b4c9fef3ac2077c34b
deepthins08 <149232424+deepthins08@users.noreply.github.com>
2024-01-05 21:14:20 +0530

    collection methods

---
+++ Intellij/collection/src/com/xworkz/collection/boot/RainbowRunner.java
@@ -0,0 +1,11 @@
+package com.xworkz.collection.boot;
+
+import com.xworkz.collection.store.Rainbow;
+
+public class RainbowRunner {
+    public static void main(String[] args) {
+
+        Rainbow rainbow=new Rainbow();
+        rainbow.getColors();
+    }
+}
